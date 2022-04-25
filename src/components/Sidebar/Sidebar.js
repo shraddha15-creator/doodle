8014388e07eae0041708beb897ea7255c642e7a3
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-04-25 12:40:40 +0530

    sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,31 @@
 import React from "react";
+import "./sidebar.css";
 
 const Sidebar = () => {
-	return <div>Sidebar</div>;
+	return (
+		<div className="sidebar-container">
+			<div className="sidebar-items">
+				<i class="fas fa-home"></i>
+				<span>Home</span>
+			</div>
+			<div className="sidebar-items">
+				<i class="fas fa-heart"></i>
+				<span>Liked</span>
+			</div>
+			<div className="sidebar-items">
+				<i class="fas fa-photo-video"></i>
+				<span>Playlist</span>
+			</div>
+			<div className="sidebar-items">
+				<i class="fas fa-clock"></i>
+				<span>Watch Later</span>
+			</div>
+			<div className="sidebar-items">
+				<i class="fas fa-history"></i>
+				<span>History</span>
+			</div>
+		</div>
+	);
 };
 
 export default Sidebar;
