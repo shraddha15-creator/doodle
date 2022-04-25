da9c1c7db49c5a37e17cc256ed6ef5a51ec2e273
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-04-25 22:12:56 +0530

    single video card added

--- src/pages/VideoListing/VideoListing.js
+++ src/pages/VideoListing/VideoListing.js
@@ -1,7 +1,171 @@
 import React from "react";
+import "./videoListing.css";
 
 const VideoListing = () => {
-	return <div>VideoListing</div>;
+	return (
+		<div className="videos-container">
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/EGLXOVhxLco/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwIHbKYPuFIWwsFvTiCawjRGIKRA"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Doodle for Beginners | Draw step by step with Me</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Artisty</h6>
+				<div className="views-and-date">
+					<h6>12.4K Views</h6>
+					<h6> 2 years ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/EGLXOVhxLco/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwIHbKYPuFIWwsFvTiCawjRGIKRA"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Doodle for Beginners | Draw step by step with Me</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Artisty</h6>
+				<div className="views-and-date">
+					<h6>12.4K Views</h6>
+					<h6> 2 years ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/5Eqb_-j3FDA/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDdRN1utUP-GsBpZe-QenMc94bEhg"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Coke Studio</h6>
+				<div className="views-and-date">
+					<h6>1.6M Views</h6>
+					<h6> 2 Months ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/5Eqb_-j3FDA/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDdRN1utUP-GsBpZe-QenMc94bEhg"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Coke Studio</h6>
+				<div className="views-and-date">
+					<h6>1.6M Views</h6>
+					<h6> 2 Months ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/5Eqb_-j3FDA/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDdRN1utUP-GsBpZe-QenMc94bEhg"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Coke Studio</h6>
+				<div className="views-and-date">
+					<h6>1.6M Views</h6>
+					<h6> 2 Months ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/EGLXOVhxLco/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwIHbKYPuFIWwsFvTiCawjRGIKRA"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Doodle for Beginners | Draw step by step with Me</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Artisty</h6>
+				<div className="views-and-date">
+					<h6>12.4K Views</h6>
+					<h6> 2 years ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/5Eqb_-j3FDA/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDdRN1utUP-GsBpZe-QenMc94bEhg"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Coke Studio</h6>
+				<div className="views-and-date">
+					<h6>1.6M Views</h6>
+					<h6> 2 Months ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/5Eqb_-j3FDA/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDdRN1utUP-GsBpZe-QenMc94bEhg"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Coke Studio</h6>
+				<div className="views-and-date">
+					<h6>1.6M Views</h6>
+					<h6> 2 Months ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/5Eqb_-j3FDA/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDdRN1utUP-GsBpZe-QenMc94bEhg"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Coke Studio</h6>
+				<div className="views-and-date">
+					<h6>1.6M Views</h6>
+					<h6> 2 Months ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+			<div className="single-video-card">
+				<img
+					src="https://i.ytimg.com/vi/5Eqb_-j3FDA/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDdRN1utUP-GsBpZe-QenMc94bEhg"
+					alt="single-vdo"
+				/>
+				<div className="video-name-and-menu">
+					<h5>Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill</h5>
+					<i class="fas fa-ellipsis-v"></i>
+				</div>
+				<h6 className="video-channel">Coke Studio</h6>
+				<div className="views-and-date">
+					<h6>1.6M Views</h6>
+					<h6> 2 Months ago</h6>
+				</div>
+			</div>
+			{/* --------------------------- */}
+		</div>
+	);
 };
 
 export default VideoListing;
