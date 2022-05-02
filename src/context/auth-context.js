fddde35ae1885b04998cda72ca0db46b3bed7fbf
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-05-02 13:31:46 +0530

    feat: authentication

---
+++ src/context/auth-context.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, createContext, useContext } from "react";
+
+const AuthContext = createContext({ token: null, user: null });
+
+const AuthProvider = ({ children }) => {
+	const [user, setUser] = useState();
+
+	useEffect(() => {
+		(async () => {
+			const token = localStorage.getItem("token");
+			const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+
+			token
+				? setUser({ token: token, userDetails: userDetails, isLoggedIn: true })
+				: setUser({ token: "", userDetails: "?", isLoggedIn: false });
+		})();
+	}, [user]);
+	console.log(user);
+
+	return (
+		<AuthContext.Provider value={{ user, setUser }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+const useAuth = () => useContext(AuthContext);
+export { useAuth, AuthProvider };
--- src/pages/Auth/Signup/Signup.js
+++ src/pages/Auth/Signup/Signup.js
@@ -1,8 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../../context/auth-context";
 import "../auth.css";
 
 const Signup = () => {
+	const navigate = useNavigate();
+	const { setUser } = useAuth();
+	const [userSignupData, setUserSignupData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+
+	const signupHandler = async (userDetails) => {
+		const { firstName, lastName, email, password } = userDetails;
+		try {
+			const response = await axios.post("/api/auth/signup", {
+				firstName,
+				lastName,
+				email,
+				password,
+			});
+			console.log(response);
+			localStorage.setItem("token", response.data.encodedToken);
+			localStorage.setItem(
+				"userDetails",
+				JSON.stringify(response.data.createdUser)
+			);
+			setUser({
+				token: response.data.encodedToken,
+				userDetails: response.data.createdUser,
+				isLoggedIn: true,
+			});
+			navigate("/");
+		} catch (error) {
+			console.error("while signing up", error);
+		}
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		userSignupData.password === userSignupData.confirmPassword
+			? signupHandler(userSignupData)
+			: console.log("Password doest match");
+	};
+
 	return (
 		<>
 			<div className="auth-container">
@@ -10,27 +55,97 @@ const Signup = () => {
 					<h2>
 						Signup to <span className="brand-name">DoodleTv 🔐</span>
 					</h2>
-					<div className="auth-inputs">
-						<fieldset>
-							<legend>Name</legend>
-							<input />
-						</fieldset>
-						<fieldset>
-							<legend>Email</legend>
-							<input />
-						</fieldset>
-						<fieldset>
-							<legend>Password</legend>
-							<input />
-						</fieldset>
-					</div>
-					<button className="auth-btn btn-filled">Signup</button>
-					<div>
-						Already have an account?
-						<Link to="/login" className="link-signup">
-							Login
-						</Link>
-					</div>
+					<form onSubmit={submitHandler}>
+						<div className="auth-inputs">
+							<fieldset>
+								<legend>First Name</legend>
+								<input
+									type="text"
+									id="firstname"
+									placeholder="First Name"
+									name="firstname"
+									value={userSignupData.firstName}
+									onChange={(e) =>
+										setUserSignupData((prev) => ({
+											...prev,
+											firstName: e.target.value,
+										}))
+									}
+								/>
+							</fieldset>
+							<fieldset>
+								<legend>Last Name</legend>
+								<input
+									type="text"
+									id="lastname"
+									placeholder="Last Name"
+									name="lastname"
+									value={userSignupData.lastName}
+									onChange={(e) =>
+										setUserSignupData((prev) => ({
+											...prev,
+											lastName: e.target.value,
+										}))
+									}
+								/>
+							</fieldset>
+							<fieldset>
+								<legend>Email</legend>
+								<input
+									type="email"
+									id="email"
+									placeholder="test@gmail.com"
+									name="email"
+									value={userSignupData.email}
+									onChange={(e) =>
+										setUserSignupData((prev) => ({
+											...prev,
+											email: e.target.value,
+										}))
+									}
+								/>
+							</fieldset>
+							<fieldset>
+								<legend>Password</legend>
+								<input
+									type="password"
+									id="password"
+									placeholder="********"
+									name="password"
+									value={userSignupData.password}
+									onChange={(e) =>
+										setUserSignupData((prev) => ({
+											...prev,
+											password: e.target.value,
+										}))
+									}
+								/>
+							</fieldset>
+							<fieldset>
+								<legend>Confirm Password</legend>
+								<input
+									type="password"
+									id="confirmPassword"
+									placeholder="********"
+									name="confirmPassword"
+									value={userSignupData.confirmPassword}
+									onChange={(e) =>
+										setUserSignupData((prev) => ({
+											...prev,
+											confirmPassword: e.target.value,
+										}))
+									}
+								/>
+							</fieldset>
+						</div>
+						<button className="auth-btn btn-filled">Signup</button>
+						<div>
+							Already have an account?
+							<Link to="/login" className="link-signup">
+								Login
+							</Link>
+						</div>
+					</form>
 				</div>
 			</div>
 		</>
