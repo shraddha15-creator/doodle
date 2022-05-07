414536c93ab5dd6b03397c4d6ade304c6839f6b3
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-05-07 22:46:44 +0530

    fetch data changes

--- src/services/deleteFromWatchLater.js
+++ src/services/deleteFromWatchLater.js
@@ -3,14 +3,14 @@ import { token } from "./token";
 
 const encodedToken = token();
 
-export const deleteFromWatchLater = async (id, watchLaterDispatch) => {
+export const deleteFromWatchLater = async (id, watchLaterDispatch, token) => {
 	try {
-		const data = await axios.delete(`/api/user/watchlater/${id}`, {
+		const response = await axios.delete(`/api/user/watchlater/${id}`, {
 			headers: { authorization: encodedToken },
 		});
 		watchLaterDispatch({
 			type: "DELETE_FROM_WATCH_LATER",
-			payload: data.data.watchlater,
+			payload: response.data.watchlater,
 		});
 	} catch (error) {
 		console.error("ERROR WHILE DELETING FROM WATCH LATER", error);
