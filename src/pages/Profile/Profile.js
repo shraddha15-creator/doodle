f5b85d234ba5a76cf7beee9f582f2581650a60e6
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-05-20 15:17:08 +0530

    fix: css deploy error

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth, useLikes, usePlaylist, useWatchLater } from "../../context";
-import "./profile.css";
+import "./Profile.css";
 
 export const Profile = () => {
 	const navigate = useNavigate();
