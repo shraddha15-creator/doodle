1fd74d1f3720d8a8b479c8a4857db861d48e65f6
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-07-01 13:06:33 +0530

    fix: removed console

--- src/services/deleteFromWatchLater.js
+++ src/services/deleteFromWatchLater.js
@@ -4,12 +4,10 @@ import { token } from "./token";
 const encodedToken = token();
 
 export const deleteFromWatchLater = async (videoId, watchLaterDispatch) => {
-	console.log(videoId, "delet watch later");
 	try {
 		const response = await axios.delete(`/api/user/watchlater/${videoId}`, {
 			headers: { authorization: encodedToken },
 		});
-		console.log(response);
 		watchLaterDispatch({
 			type: "DELETE_FROM_WATCH_LATER",
 			payload: response.data.watchlater,
